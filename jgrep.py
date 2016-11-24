1ec0c94abde0378fb7cfad0ef780671e2401412a
David Pärsson <david@parsson.se>
2016-11-24 13:35:25 +0100

    Better prevention of duplicate jobs

--- jgrep.py
+++ jgrep.py
@@ -59,15 +59,19 @@ def grep_builds(arguments, pattern, job):
 
 
 def recursive_jobs(job_or_view):
+    jobs = set()
     if job_or_view.builds:
-        yield job_or_view
+        jobs.add(job_or_view)
     for job in job_or_view.jobs or []:
         for child_job in recursive_jobs(job):
-            yield child_job
+            jobs.add(child_job)
     for view in job_or_view.views or []:
         if view.get_url() != job_or_view.get_url():
-            for child_job in recursive_jobs(job):
-                yield child_job
+            for child_job in recursive_jobs(view):
+                jobs.add(child_job)
+    for child_job in sorted(jobs):
+        yield child_job
+
 
 if __name__ == '__main__':
     try:
